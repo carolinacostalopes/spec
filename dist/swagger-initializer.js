6e022e863f23f57273d81ac4a0bc50ea90b6e36e
carolinacostalopes <36004237+carolinacostalopes@users.noreply.github.com>
2023-10-23 08:52:44 +0000

    chore: try multiple versions

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,10 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    urls: [
+      {url: "swagger.yaml", name: "Version One"},
+      {url: "swaggerv2.yaml", name: "Version Two"},
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
